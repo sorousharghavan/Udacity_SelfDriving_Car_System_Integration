f98538e0bd3a6f0a903248de8517985c7c35f7dd
huboqiang <huboqiang@gmail.com>
2017-10-01 00:55:17 +0800

    A very first version, keep speeding up forward.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -4,9 +4,11 @@ ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+    def __init__(self, **kwargs):
+        self.map_carFeatures = {}
+        for key in kwargs:
+            self.map_carFeatures[key] = kwargs[key]
+
 
     def control(self, *args, **kwargs):
         # TODO: Change the arg, kwarg list to suit your needs
