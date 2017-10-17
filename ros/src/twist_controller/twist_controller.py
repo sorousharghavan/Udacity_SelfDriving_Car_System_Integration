b397b25b749a8237c45446591ae17b254883b02e
huboqiang <huboqiang@gmail.com>
2017-10-17 20:02:42 +0800

    Provides a higher weight on returned brake, which took the total weight of the car, wheel radius into consideration. The returned brake value were than filtered, firstly for removing low brake value, then filtered using lowpass filter.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -2,6 +2,7 @@
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 import rospy
+import time
 
 class Controller(object):
     def __init__(self, **kwargs):
@@ -9,9 +10,11 @@ class Controller(object):
         for key in kwargs:
             self.map_carFeatures[key] = kwargs[key]
 
+        self.last_update_t = time.time()
+
 
     def control(self, target_speed, target_vangular,
-            current_velocity, d, pid_throttle, pid_steerangel, yaw_steerangel
+            current_velocity, d, pid_throttle, pid_steerangel, lpf_brake, yaw_steerangel
         ):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
@@ -19,20 +22,27 @@ class Controller(object):
         current_angle = current_velocity.twist.angular.z
 
         cte_speed = current_speed - target_speed
-        car_throttle = pid_throttle.step(cte_speed, 1/50.0)
-        car_steerangle = pid_steerangel.step(d, 1/50.0)
-
-        #car_steerangle += yaw_steerangel.get_steering(
-    #        target_speed, target_vangular, current_speed
-    #    )
+        current_t = time.time()
+        delta_t = current_t - self.last_update_t
+        self.last_update_t = current_t
+        car_throttle = pid_throttle.step(cte_speed, delta_t)
+        car_steerangle = pid_steerangel.step(d, delta_t)
 
         throttle = 0
         brake = 0
         if car_throttle > 0:
             throttle = car_throttle
         else:
+            if car_throttle >-0.1:
+                car_throttle = 0.0
+
             brake = -1*car_throttle
 
+        max_car_mass = (self.map_carFeatures['vehicle_mass']+self.map_carFeatures['fuel_capacity']*GAS_DENSITY)
+        brake_max = max_car_mass*-1*self.map_carFeatures['decel_limit']*self.map_carFeatures['wheel_radius']
+        brake = 0.5 * brake_max * brake
+        brake = lpf_brake.filt(brake)
+        rospy.logwarn("[%f, %f, %f, %f]" % (current_speed, target_speed, throttle, brake))
         #rospy.logwarn("BEGIN_ctrl_throttle===")
         #rospy.logwarn(current_velocity.twist.linear.x)
         #rospy.logwarn(cte_speed)
